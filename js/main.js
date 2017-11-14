575615770891d20a4330ebef6ae112cc1b582779
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-14 16:41:55 -0500

    completed assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+// variables
+const status = document.querySelector('h1.status');
+const button = document.querySelector('.plate button');
+const body = document.querySelector('body');
 
 
+
+
+// light switch functions
+  button.addEventListener('click', function functionName() {
+    if (this.className === "switch on") {
+      status.innerText = "Hey, who turned off the lights?";
+      button.className = "switch off";
+      body.className = "dark";
+
+    }else if (this.className === "switch off" ) {
+      status.innerText = "It's so bright in here!";
+      button.className = "switch on";
+      body.className = "light";
+    };
+
+  });
+
+
+
+
+
+
+});
